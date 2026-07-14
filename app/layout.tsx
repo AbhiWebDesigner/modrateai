e4830c2fb98ad6c61bc341bbdb1981bb7e78afde
AbhiWebDesigner <275483561+AbhiWebDesigner@users.noreply.github.com>
2026-07-14 22:12:15 +0530

    full upgrde logo version

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     description: "Stop toxic comments automatically in every language.",
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.png",
   },
 };
 
